533e622f43419c8adba7111474ba03e29b26c5e9
premiering <premieringsalt@protonmail.com>
2024-05-14 21:23:56 -0400

    add timely leaderboards

--- leaderboard.js
+++ leaderboard.js
@@ -4,12 +4,62 @@ const lbContainer = document.getElementById(playerContId);
 
 const lbPlayerLimit = 35;
 
-const buttonToTrackedArea = new Map();
-buttonToTrackedArea.set("exodus-lb", "Exodus");
-buttonToTrackedArea.set("spaceadv-lb", "Space Advanced");
-buttonToTrackedArea.set("infernus-lb", "Infernus");
-buttonToTrackedArea.set("inferno-lb", "Inferno");
-buttonToTrackedArea.set("nightmare-lb", "Nightmare");
+const completionBasedButton = document.getElementById("completion-based-btn");
+const timelyBasedButton = document.getElementById("time-based-btn");
+
+const currentlyViewingP = document.getElementById("currently-viewing");
+
+const buttonToCompletionLevel = new Map();
+buttonToCompletionLevel.set("exodus-lb", "Exodus");
+buttonToCompletionLevel.set("spaceadv-lb", "Space Advanced");
+buttonToCompletionLevel.set("infernus-lb", "Infernus");
+buttonToCompletionLevel.set("inferno-lb", "Inferno");
+buttonToCompletionLevel.set("nightmare-lb", "Nightmare");
+
+const buttonToTimelyArea = new Map();
+buttonToTimelyArea.set("exodus50-lb", "Exodus 50 VICTORY");
+buttonToTimelyArea.set("exodus100-lb", "Exodus 100 VICTORY");
+buttonToTimelyArea.set("exodus150-lb", "Exodus 150 VICTORY");
+buttonToTimelyArea.set("spaceadv20-lb", "Space Advanced 20 VICTORY");
+buttonToTimelyArea.set("infernus25-lb", "Infernus 25");
+buttonToTimelyArea.set("inferno25-lb", "Inferno 25");
+buttonToTimelyArea.set("nightmare20-lb", "Nightmare 20");
+
+const TYPE_COMPLETION = 0;
+const TYPE_TIMELY = 1;
+
+let currentLeaderboardType = TYPE_TIMELY;
+
+function setCurrentType(type) {
+    if (type != TYPE_COMPLETION && type != TYPE_TIMELY)
+        return;
+
+    if (type == TYPE_COMPLETION) {
+        loadCompletionLeaderboards("Exodus");
+        showButtons(buttonToCompletionLevel);
+        hideButtons(buttonToTimelyArea);
+    } else {
+        loadTimelyLeaderboards("Exodus 100 VICTORY");
+        showButtons(buttonToTimelyArea);
+        hideButtons(buttonToCompletionLevel);
+    }
+}
+
+function hideButtons(buttonToXMap) {
+    for (let [button, _] of buttonToXMap) {
+        document.getElementById(button).hidden = true;
+    }
+}
+
+function showButtons(buttonToXMap) {
+    for (let [button, _] of buttonToXMap) {
+        document.getElementById(button).hidden = false;
+    }
+}
+
+function setCurrentlyViewing(s) {
+    currentlyViewingP.textContent = "Currently viewing " + s + ".";
+}
 
 function clearLbContainer() {
     lbContainer.textContent = '';
@@ -23,11 +73,12 @@ function addParagraphToLb(text, ...classes) {
     lbContainer.appendChild(p);
 }
 
-function loadAreaLeaderboards(area) {
-    fetch(tokeiUrl + "/api/leaderboard/completion?area=" + area).catch((err) => {
+function loadCompletionLeaderboards(level) {
+    setCurrentlyViewing(level);
+    fetch(tokeiUrl + "/api/leaderboard/completion?area=" + level).catch((err) => {
         console.log(err);
         clearLbContainer();
-        addParagraphToLb("Failed to load tokei leaderboard.");
+        addParagraphToLb("Failed to load completion leaderboard.");
     }).then((resp) => {
         return resp.json();
     }).then((data) => {
@@ -45,13 +96,63 @@ function loadAreaLeaderboards(area) {
             playersAdded++;
             addParagraphToLb(`#${playersAdded} ${player.playerName} | ${player.areaReached} | ${localDate.toLocaleString()}`, "normal-placement");
         }
+        if (playersAdded == 0) {
+            addParagraphToLb("No one has played this level!");
+        }
     });
 }
 
-for (let [button, areaName] of buttonToTrackedArea) {
+function loadTimelyLeaderboards(area) {
+    setCurrentlyViewing(area);
+    fetch(tokeiUrl + "/api/leaderboard/timely?area=" + area).catch((err) => {
+        console.log(err);
+        clearLbContainer();
+        addParagraphToLb("Failed to load timely leaderboard.");
+    }).then((resp) => {
+        return resp.json();
+    }).then((data) => {
+        if (!data.placements)
+            return;
+        clearLbContainer();
+        let playersAdded = 0;
+        for (let player of data.placements) {
+            if (playersAdded >= lbPlayerLimit)
+                return;
+
+            let localDate = new Date(player.dateReached);
+            // Localize the date from UTC
+            localDate = new Date(localDate - localDate.getTimezoneOffset() * 60000);
+            playersAdded++;
+            addParagraphToLb(`#${playersAdded} ${player.playerName} | ${msToTime(player.timeReachedMs)}`, "normal-placement");
+        }
+        if (playersAdded == 0) {
+            addParagraphToLb("No one has reached this area!");
+        }
+    });
+}
+
+function msToTime(s) {
+    var pad = (n, z = 2) => ('00' + n).slice(-z);
+    return pad(s/3.6e6|0) + ':' + pad((s%3.6e6)/6e4 | 0) + ':' + pad((s%6e4)/1000|0) + '.' + pad(s%1000, 3);
+}
+
+for (let [button, levelName] of buttonToCompletionLevel) {
     document.getElementById(button).addEventListener('click', () => {
-        loadAreaLeaderboards(areaName);
+        loadCompletionLeaderboards(levelName);
     });
 }
 
-loadAreaLeaderboards("Exodus");
+for (let [button, levelName] of buttonToTimelyArea) {
+    document.getElementById(button).addEventListener('click', () => {
+        loadTimelyLeaderboards(levelName);
+    });
+}
+
+completionBasedButton.addEventListener("click", () => {
+    setCurrentType(TYPE_COMPLETION);
+});
+timelyBasedButton.addEventListener("click", () => {
+    setCurrentType(TYPE_TIMELY);
+});
+
+setCurrentType(TYPE_TIMELY);
